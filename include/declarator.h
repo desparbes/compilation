6e3140900de87a43ef14c089d6f0a558f09a186b
Mr Hyk <omar.hayak@enseirb.fr>
2015-12-19 14:21:24 +0100

    fist try

--- include/declarator.h
+++ include/declarator.h
@@ -21,26 +21,26 @@ void declarator_identifier(gen_t* $$, char* $1) {
 }
 
 void declarator_array(gen_t* $$, gen_t* $1, int $3) {
-  $$->type = last_type;
+  $$->type = $1->type;
   $$->var = $1->var;
   asprintf(&$$->code, "%s = alloca [%d x %s]\n", $$->var, $3, get_type($1->type));
 }
 
 void declarator_array_ptr(gen_t* $$, gen_t* $1) {
-  $$->type = last_type;
+  $$->type = $1->type;
   $$->var = $1->var;
   asprintf(&$$->code, "%s = alloca %s*\n", $$->var, get_type($1->type));
 }
 
 // prototypes
 void declarator_function_param(gen_t* $$, gen_t* $1, gen_t* $3) {
-  $$->type = last_type;
+  $$->type = $1->type;
   $$->var = $1->var;
   asprintf(&$$->code, "%s(%s)\n", $1->var, $3->code);
 }
 
 void declarator_function_void(gen_t* $$, gen_t* $1) {
-  $$->type = last_type;
+  $$->type = $1->type;
   $$->var = $1->var;
   asprintf(&$$->code, "%s(void)\n", $1->var);
 }
